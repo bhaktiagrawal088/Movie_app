85ef3828cc8b2ad2b15bcd5afa1ef6993c0dda82
unknown <bhaktiagrawal397@gmail.com>
2024-07-23 00:05:30 +0530

    Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-
-import Navbar from './Component/Navbar/Navbar'
-
+import {Routes, Route} from 'react-router-dom'
+import Home from "./pages/Home/Home"
 function App() {
 
   return (
     <>
-      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </>
   )
 }
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+import Navbar from "../../Component/Navbar/Navbar";
+import MovieCard from "../../Component/MovieCard/MovieCard";
+import { useDispatch, useSelector } from "react-redux";
+import { getMovies } from "../../API/getMovies";
+
+function Home() {
+  const dispatch = useDispatch();
+  const movies = useSelector((state) => state.movies.movies);
+
+  // const [selectedMovieId, setSelectedMovieId] = useState(null);
+
+  useEffect(() => {
+    dispatch(getMovies());
+  }, []);
+
+  return (
+    <div>
+      <Navbar />
+      <main className=" grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 p-4 ">
+      {
+        movies?.length > 0 ? (
+          movies.map((movie) => (
+            <MovieCard key={movie.id} movie={movie} />
+          ))
+        ) : (
+          {/* <p>No movies available</p> */}
+        )
+      } 
+      </main>
+    </div>
+  );
+}
+
+export default Home;
